e84366121a12fb83754d2b36225b9068d6c9a152
Diogo Goncalves <diogo@ticketmates.com.au>
2020-09-30 10:45:31 +1000

    Move button css to index

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,13 +7,13 @@ function Hero({data, isLoading, list}) {
     		<div className="hero__info">
     			<h2 className="hero__title">{data ? data.original_title : ''}</h2>
     			<p className="hero__desc">{data ? data.tagline : ''}</p>
-    			<div className="hero__btns">
-    				<button className="hero__btn hero__btn--play">
-    					<img className="hero__icon hero__icon--play" src={require('../images/streamline-icon-controls-play@15x15.png')} alt=""/>
+    			<div className="btns">
+    				<button className="btn btn--play">
+    					<img className="btn__icon btn__icon--play" src={require('../images/streamline-icon-controls-play@15x15.png')} alt=""/>
     					Play
     				</button>
-    				<button className="hero__btn hero__btn--moreinfo">
-    					<span className="hero__icon hero__icon--info">ⓘ</span>
+    				<button className="btn btn--moreinfo">
+    					<span className="btn__icon btn__icon--info">ⓘ</span>
     					More info
     				</button>
     			</div>
